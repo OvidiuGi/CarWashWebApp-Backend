4fc935471cdd9a172740cee37d8aa85789c21446
Romulus Andrei Bacila <30293758+Tolsi111@users.noreply.github.com>
2022-11-07 21:10:06 +0200

    init

---
+++ src/test/java/com/example/carwashwebappbackend/CarWashWebAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carwashwebappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWashWebAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
